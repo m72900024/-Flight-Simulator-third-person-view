6f4a68addf72600409adeb7bf9a9bb1c167a353d
m72900024 <m72900024@gmail.com>
2026-01-21 09:39:06 +0800

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,27 +1,20 @@
 export const CONFIG = {
-    // 物理參數
     gravity: 9.81,
     mass: 0.6,
     maxThrust: 28.0,
     dragCoeff: 0.05,
-    hardDeck: 0.05, // 地板高度
-
-    // 飛行手感
-    rates: 1.0,         // 靈敏度
-    thrustPower: 35,    // 動力大小
+    hardDeck: 0.05,
     
-    // 搖桿映射 (可依需求修改)
-    axes: {
-        roll: 0,
-        pitch: 1,
-        thrust: 3,
-        yaw: 4,
-        arm: 2
-    },
+    thrustPower: 35,
+    rates: 1.0,
+
+    axes: { roll: 0, pitch: 1, thrust: 3, yaw: 4, arm: 2 },
+    
+    // 反轉設定 (會在 Input.js 中動態讀取)
     invert: {
-        roll: false,
-        pitch: true,  // 通常 pitch 需要反轉
-        thrust: false, // 視搖桿而定，有的需要反轉
-        yaw: false
+        t: false,
+        r: false,
+        p: true,  // 預設 Pitch 反轉
+        y: false
     }
 };
